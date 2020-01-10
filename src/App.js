999d26ec324532084905bd0425ce1e58cac1c03a
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-10 13:28:47 -0500

    finished add-col

--- src/App.js
+++ src/App.js
@@ -3,102 +3,117 @@ import "./App.css";
 import { Table } from "./components";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultColor: "#222222",
-      currentColor: "#222222",
-      colorArray: [],
-      numRows: 2,
-      numCols: 3
-    };
-  }
+	constructor(props) {
+		super(props);
 
-  componentDidMount = () => {
-    const row = Array.from({ length: this.state.numCols }).map(
-      x => this.state.defaultColor
-    );
-    const array = Array.from({ length: this.state.numRows }).map(x => row);
-    this.setState(
-      {
-        colorArray: array
-      },
-      () => console.log(this.state.colorArray)
-    );
-  };
+		const defaultColor = "#222222";
+		const initialRow = 2;
+		const initialCol = 3;
+		const row = Array.from({ length: initialCol }).map(x => defaultColor);
+		const array = Array.from({ length: initialRow }).map(x => row);
 
-  addRow = () => {
-    const newRow = Array.from({ length: this.state.numCols }).map(
-      x => this.state.defaultColor
-    );
-    this.setState(
-      {
-        numRows: this.state.numRows + 1,
-        colorArray: this.state.colorArray.concat([newRow])
-      },
-      () => console.log(this.state.colorArray)
-    );
-  };
+		this.state = {
+			defaultColor: defaultColor,
+			currentColor: "#222222",
+			colorArray: array
+		};
+	}
 
-  addCol = () => {};
+	// componentDidMount = () => {
+	// 	const initialRow = 2;
+	// 	const initialCol = 3;
+	// 	const row = Array.from({ length: initialCol }).map(
+	// 		x => this.state.defaultColor
+	// 	);
+	// 	const array = Array.from({ length: initialRow }).map(x => row);
+	// 	this.setState(
+	// 		{
+	// 			colorArray: array
+	// 		},
+	// 		() => console.log(this.state.colorArray)
+	// 	);
+	// };
 
-  removeRow = () => {
-    if (this.state.numRows > 1) {
-      let newArr = this.state.colorArray.slice(0, -1);
-      this.setState(
-        {
-          numRows: this.state.numRows - 1,
-          colorArray: newArr
-        },
-        () => console.log(this.state.colorArray)
-      );
-    } else {
-      alert("Minimum rows achieved");
-    }
-  };
+	addRow = () => {
+		const newRow = Array.from({ length: this.state.colorArray[0].length }).map(
+			x => this.state.defaultColor
+		);
+		this.setState(
+			{
+				colorArray: this.state.colorArray.concat([newRow])
+			},
+			() => console.log(this.state.colorArray)
+		);
+	};
 
-  removeCol = () => {};
+	addCol = () => {
+		let newArr = this.state.colorArray.map(row => {
+			return row.concat(this.state.defaultColor);
+		});
+		this.setState({
+			colorArray: newArr
+		});
+	};
 
-  updateSelectedColor = e => {
-    this.setState({
-      currentColor: e.target.value
-    });
-  };
+	removeRow = () => {
+		if (this.state.colorArray.length > 1) {
+			let newArr = this.state.colorArray.slice(0, -1);
+			this.setState(
+				{
+					colorArray: newArr
+				},
+				() => console.log(this.state.colorArray)
+			);
+		} else {
+			alert("Minimum rows achieved");
+		}
+	};
 
-  fillAll = () => {};
+	removeCol = () => {};
 
-  clearAll = () => {};
+	updateSelectedColor = e => {
+		this.setState({
+			currentColor: e.target.value
+		});
+	};
 
-  fillUncolored = () => {};
+	fillAll = () => {};
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.addRow}>Add a row</button>
-        <button onClick={this.addCol}>Add a col</button>
-        <button onClick={this.removeRow}> Remove a row</button>
-        <button onClick={this.removeCol}> Remove a row</button>
+	clearAll = () => {};
 
-        <select
-          value={this.state.currentColor}
-          onChange={this.updateSelectedColor}
-        >
-          <option id="default" value="#222222">
-            default
-          </option>
-          <option id="algae" value="#FFFFFF">
-            algae
-          </option>
-        </select>
+	fillUncolored = () => {};
 
-        <Table numRows={this.state.numRows} numCols={this.state.numCols} />
+	render() {
+		return (
+			<div>
+				<button onClick={this.addRow}>Add a row</button>
+				<button onClick={this.addCol}>Add a col</button>
+				<button onClick={this.removeRow}> Remove a row</button>
+				<button onClick={this.removeCol}> Remove a row</button>
 
-        <button onClick={this.fillAll}>Fill all</button>
-        <button onClick={this.clearAll}>Clear all</button>
-        <button onClick={this.fillUncolored}>Fill only uncolored</button>
-      </div>
-    );
-  }
+				<select
+					value={this.state.currentColor}
+					onChange={this.updateSelectedColor}
+				>
+					<option id="default" value="#222222">
+						default
+					</option>
+					<option id="algae" value="#FFFFFF">
+						algae
+					</option>
+				</select>
+
+				<Table
+					numRows={this.state.colorArray.length}
+					numCols={this.state.colorArray[0].length}
+				/>
+
+				<button onClick={this.fillAll}>Fill all</button>
+				<button onClick={this.clearAll}>Clear all</button>
+				<button onClick={this.fillUncolored}>Fill only uncolored</button>
+			</div>
+		);
+	}
 }
 
 export default App;
